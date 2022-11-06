174fc5b1b4a244cfc3276011656c70fb94d7b91a
root <root@DESKTOP-JFEPESN.localdomain>
2022-11-06 21:09:32 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here
+class Breakfast{
+    constructor(food, drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+class Dinner{
+    #dessert
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.#dessert = dessert
+    }
+}
+
+
+
+// Initialize dessert as a private property by prefixing its name with the hash symbol (#).
